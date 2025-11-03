64412dc6c534829b87fb3a053f365938517bbe70
mintcolon3 <143480965+mintcolon3@users.noreply.github.com>
2025-11-03 17:54:01 +0000

    bwuu

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,7 @@ export default function DashboardLayout({ children }) {
         <title>MintColon3</title>
         <meta content="MintColon3" property="og:title" />
         <meta content="Mint • She/Her • 🏳️‍⚧️🇬🇧" property="og:description" />
-        <meta content="/" property="og:url" />
-        <meta content="./favicon.png" property="og:image" />
+        <meta content="https://mintcolon3.github.io/favicon.png" property="og:image" />
         <meta content="#e4a0ff" data-react-helmet="true" name="theme-color" />
         <link rel="icon" type="image/x-icon" href="./favicon.png"></link>
       </head>
